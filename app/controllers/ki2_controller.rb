93b68778d1199eeed74d0813a05002915334a295
Vladimir Lebedev-Schmidthof <dair@albiongames.org>
2017-08-24 12:29:37 +0300

    Year/region selection looks good

--- app/controllers/ki2_controller.rb
+++ app/controllers/ki2_controller.rb
@@ -6,9 +6,24 @@ class Ki2Controller < ApplicationController
             @regions = Ki2.regions()
         end
     end
+    
+    def fillYearsCache
+        unless @years_cache
+            @years_cache = Ki2.yearsCache()
+        end
+    end
+
+    def yearFromParam(p)
+        if /^\d\d\d\d$/.match(p) then
+            return p.to_i
+        else
+            return Time.now.year
+        end
+    end
 
     def games
         fillRegions()
+        fillYearsCache()
 
         @region_key = ''
         @region_name = 'Все'
@@ -16,17 +31,17 @@ class Ki2Controller < ApplicationController
         if params[:key] then
             if @regions.key?(params[:key]) then
                 @region_key = params[:key]
-            elsif /^\d\d\d\d$/.match(params['key']) then
-                @year = params[:key].to_i
+            else
+                @year = yearFromParam(params[:key])
             end
         elsif params[:loc] != nil and params[:year] != nil then
             @region_key = params[:loc]
-            @year = params[:year]
+            @year = yearFromParam(params[:year])
         end
         
         @year = Time.now.year unless @year
 
-        @region_name = @regions[@region_key]
+        @region_name = @regions[@region_key][:name]
     end
 
 end
--- app/models/ki2.rb
+++ app/models/ki2.rb
@@ -1,12 +1,43 @@
 class Ki2 < ActiveRecord::Base
     def self.regions()
-        rows = connection.select_all("select region_code, region_name from ki_regions order by region_id asc")
+        rows = connection.select_all("select region_id, region_code, region_name from ki_regions order by region_id asc")
         
         ret = {}
 
         rows.to_hash.each do |row|
-            ret[row["region_code"]] = row["region_name"]
+            ret[row["region_code"]] = {:name => row["region_name"], :id => row["region_id"]}
         end
+
+        return ret
+    end
+
+    def self.yearsCache()
+        rows = connection.select_all("select year, region_id from ki_years_cache")
+
+        ret = {}
+
+        ret[1] = []
+        rows.to_hash.each do |row|
+            #unless ret.key?(row["year"])
+            #    ret[row["year"]] = []
+            #end
+            #ret[row["year"]].push(row["region_id"])
+
+
+            unless ret.key?(row["region_id"])
+                ret[row["region_id"]] = []
+            end
+            ret[row["region_id"]].push(row["year"])
+            ret[1].push(row["year"])
+        end
+
+        puts ret
+
+        ret.each do |key, value|
+            value.sort!
+            value.uniq!
+        end
+        puts ret
 
         return ret
     end
